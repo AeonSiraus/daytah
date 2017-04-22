65ba7acda8da9cb9758902b978271ff126f7dd4d
Aeon <birgetok@std.iyte.edu.tr>
2017-04-22 15:39:16 +0300

    Setup added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='daytah'
+    version='1.0a5'
+    author='AeonSiraus'
+    url='https://github.com/AeonSiraus/daytah'
+    packages=['daytah']
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas']
+)
